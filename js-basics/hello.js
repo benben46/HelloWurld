88003510f8512fdbb39f1429a2330ea5e20e8e98
benpu <pwc123451@gmail.com>
2023-03-16 11:10:13 -0400

    notes for JS Tutorial for beginners with Mosh

---
+++ js-basics/hello.js
@@ -0,0 +1,9 @@
+/* JS was originally only ran on the browser, where an embedded JS Engine ran the code. FireFox used one called SpiderMonkey and Chrome used one called V8.
+But that was eventually moved into a Node which allowed for JS to be ran outside of the web. This means that we can also use JS for backend.
+
+ECMAscript... Javascript standards
+
+ */
+
+console.log("Hello World");
+alert("yo");
